e735e60e45140ed43ca60b1fab3cc5322d3ba530
santa <ryancurtis998@yahoo.com>
2019-04-08 08:23:08 +0300

    updated credentials test

---
+++ credentials_test.py
@@ -0,0 +1,27 @@
+import unittest
+from credentials import Credential
+class TestCredential(unittest.TestCase):
+
+    """
+    Test class that defines test cases for the credential class behaviours,
+    the arguments help in creating test cases.
+
+    """
+
+    def setUp(self):
+
+        """
+        this method runs before each test case, carries the instrctions of what is to be done
+
+        """
+
+        self.new_password = Credential("ryan")
+
+    def test_init(self):
+
+        """
+        used to test if the objects have been initialized properly
+
+        """
+
+        self.assertEqual(self.new_password.credential_detail,"ryan")
